1fd5b731e7705fbca6e1c57585f96201d19d7691
aynurehuseynova <sd7juqvay@code.edu.az>
2024-02-09 23:31:34 +0400

    Location Page

--- assets/js/addtocart.js
+++ assets/js/addtocart.js
@@ -55,3 +55,87 @@ function removeSingleItem(index) {
   // Sepeti güncelle
   displayCart();
 }
+
+// const wishlistContainer = document.getElementById("wishlist");
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   displayWishlist();
+// });
+
+// function addToWishlist(product) {
+//   let wishlist = JSON.parse(localStorage.getItem("wishlist")) || [];
+
+//   // Ürün zaten istek listesinde mi kontrol et
+//   const existingItemIndex = wishlist.findIndex(item => item.id === product.id);
+
+//   if (existingItemIndex !== -1) {
+//     console.log("Ürün zaten istek listesinde.");
+//     return;
+//   }
+
+//   // Ürünü istek listesine ekle
+//   wishlist.push(product);
+//   localStorage.setItem("wishlist", JSON.stringify(wishlist));
+
+//   console.log("Ürün istek listesine eklendi.");
+//   displayWishlist();
+// }
+
+// function displayWishlist() {
+//   let wishlist = JSON.parse(localStorage.getItem("wishlist")) || [];
+
+//   if (wishlist.length === 0) {
+//     wishlistContainer.innerHTML = "<p>İstek listeniz boş.</p>";
+//     return;
+//   }
+
+//   wishlistContainer.innerHTML = `
+//     <h2>İstek Listeniz</h2>
+//     <ul>
+//       ${wishlist.map((item, index) => `
+//         <li>
+//           <img src="${item.image}" alt="${item.name}">
+//           <p>${item.name}</p>
+//           <p>Fiyat: $${item.price}</p>
+//           <button onclick="removeFromWishlist(${index})">İstekten Kaldır</button>
+        
+//         </li>`).join('')}
+//     </ul>
+//   `;
+// }
+
+// function removeFromWishlist(index) {
+//   let wishlist = JSON.parse(localStorage.getItem("wishlist")) || [];
+
+//   wishlist.splice(index, 1);
+//   localStorage.setItem("wishlist", JSON.stringify(wishlist));
+
+//   console.log("Ürün istek listesinden kaldırıldı.");
+//   displayWishlist();
+// }
+
+// function moveToCart(index) {
+//   let wishlist = JSON.parse(localStorage.getItem("wishlist")) || [];
+//   let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+//   const selectedItem = wishlist[index];
+//   wishlist.splice(index, 1);
+
+
+//   if (existingItemIndex !== -1) {
+//     // Eğer ürün sepette varsa sadece miktarını artır
+//     cart[existingItemIndex].quantity++;
+//   } else {
+//     // Eğer ürün sepette yoksa, yeni bir öğe olarak ekleyin
+//     selectedItem.quantity = 1;
+//     cart.push(selectedItem);
+//   }
+
+//   // Güncellenmiş listeleri localStorage'e kaydet
+//   localStorage.setItem("wishlist", JSON.stringify(wishlist));
+//   localStorage.setItem("cart", JSON.stringify(cart));
+
+//   console.log("Ürün sepete eklendi.");
+//   displayWishlist();
+//   displayCart();
+// }
